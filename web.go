1273a3e6041e4e039c071af67a784a6646490304
Takashi Toyoshima <toyoshim@gmail.com>
2016-01-27 17:37:00 +0000

    Add a simple log

--- web.go
+++ web.go
@@ -6,11 +6,18 @@ import (
   "os"
 )
 
+func Log(handler http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+    handler.ServeHTTP(w, r)
+  })
+}
+
 func main() {
   http.Handle("/tmalib/", http.StripPrefix("/tmalib/", http.FileServer(http.Dir("tmalib"))))
   http.Handle("/", http.RedirectHandler("/tmalib/mozapp/chrome_logo.html", 301))
   fmt.Println("listening...")
-  err := http.ListenAndServe(":" + os.Getenv("PORT"), nil)
+  err := http.ListenAndServe(":" + os.Getenv("PORT"), Log(http.DefaultServeMux))
   if err != nil {
     panic(err)
   }
