c7d4d2fbb228e31ad0c8c5ded1bd13998a279930
Takashi Toyoshima <toyoshim@gmail.com>
2016-01-27 19:19:10 +0000

    Add a momolog equivalent log to stdout

--- web.go
+++ web.go
@@ -4,12 +4,47 @@ import (
   "fmt"
   "net/http"
   "os"
+  "time"
 )
+
+type wrappedWriter struct {
+  http.ResponseWriter
+  status int
+  length int
+}
+
+func (w *wrappedWriter) WriteHeader(status int) {
+  w.status = status
+  w.ResponseWriter.WriteHeader(status)
+}
+
+func (w *wrappedWriter) Write(b []byte) (int, error) {
+  if w.status == 0 {
+    w.status = 200
+  }
+  w.length = len(b)
+  return w.ResponseWriter.Write(b)
+}
 
 func Log(handler http.Handler) http.Handler {
   return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    fmt.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
-    handler.ServeHTTP(w, r)
+    writer := wrappedWriter{ w, 0, 0 }
+    start := time.Now()
+    handler.ServeHTTP(&writer, r)
+    duration := time.Now().Sub(start)
+    fmt.Printf("%s %s %s %s %s %s %s %d %d %f %s - %s\n",
+        time.Now().Format(time.RFC3339),
+        r.Header["Referer"][0],
+        r.Method,
+        r.Host,
+        r.URL.Path,
+        r.Proto,
+        r.Header["Accept-Language"][0],
+        writer.status,
+        writer.length,
+        duration.Seconds() * 1000,
+        r.RemoteAddr,
+        r.UserAgent())
   })
 }
 
