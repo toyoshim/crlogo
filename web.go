8e1c12458d1efdc10038b2bf44426269ae02a8d0
Takashi Toyoshima <toyoshim@gmail.com>
2020-11-14 05:11:53 +0900

    remove gomolog

--- web.go
+++ web.go
@@ -5,17 +5,18 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/toyoshim/gomolog"
+//	"github.com/toyoshim/gomolog"
 )
 
 func main() {
-	log := gomolog.Open(os.Getenv("MONGOLAB_URI"), "log")
-	defer log.Close()
+//	log := gomolog.Open(os.Getenv("MONGOLAB_URI"), "log")
+//	defer log.Close()
 
 	http.Handle("/tmalib/", http.StripPrefix("/tmalib/", http.FileServer(http.Dir("tmalib"))))
 	http.Handle("/", http.RedirectHandler("/tmalib/mozapp/chrome_logo.html", 301))
 	fmt.Println("listening on port " + os.Getenv("PORT") + "...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), log.Logger())
+//	err := http.ListenAndServe(":"+os.Getenv("PORT"), log.Logger())
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
